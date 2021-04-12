0e1af049bc704b7b79083ce503105f3098485b33
ArtichOwO <leo.grange@icloud.com>
2021-04-12 13:55:28 +0200

    Updated `build/icon.ico`

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
-      preload: './src/preload.js'
+      preload: './src/preload.js',
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+      }
     },
   },
 };
